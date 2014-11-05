10a98ec586b8998a515d14befacbbfc3f3aefef7
Zain Khan <zain.khan@msn.com>
2014-11-05 00:22:23 -0500

    OPEN project dev version 1.0

--- OPEN/settings.py
+++ OPEN/settings.py
@@ -26,7 +26,9 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
