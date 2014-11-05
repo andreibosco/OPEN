3070462cc49d6786190e1334022f955157a4e703
Zain Khan <zain.khan@msn.com>
2014-11-05 01:25:27 -0500

    Settings file

--- OPEN/settings.py
+++ OPEN/settings.py
@@ -26,10 +26,6 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -101,6 +97,10 @@ DATABASES = {
 	'PASSWORD': '123456',
     }
 }
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
