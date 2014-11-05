88cfe69309262fb4368693ce4baeefd300f38339
Zain Khan <zain.khan@msn.com>
2014-11-05 00:40:57 -0500

    Database changes

--- OPEN/settings.py
+++ OPEN/settings.py
@@ -26,8 +26,13 @@ DEBUG = True
 
 TEMPLATE_DEBUG = True
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 ALLOWED_HOSTS = ['*']
 
 
@@ -88,14 +93,14 @@ WSGI_APPLICATION = 'OPEN.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'OPENdb', #os.path.join(BASE_DIR, 'OPENdb'),
-	'USER': 'zain',
-	'PASSWORD': '123456',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+ #       'NAME': 'OPENdb', #os.path.join(BASE_DIR, 'OPENdb'),
+#	'USER': 'zain',
+#	'PASSWORD': '123456',
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
